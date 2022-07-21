a738bc7cd79f588a84aaef7fe39a9058ab679a7b
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-07-21 16:21:41 -0700

    update

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/ethwu/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/ethwu/keymap.c
@@ -20,8 +20,9 @@ enum LayerNames {
     MACOS,
     WIN,
     DSC,
+    HFN,
     TFN,
-    FN
+    SFN
 };
 
 /* #define WIN_RED RGB_RED */
@@ -36,57 +37,115 @@ enum LayerNames {
 
 #define DSC_BLURPLE 0x58, 0x65, 0xf2
 
-#define TFN_COLOR 0x12, 0x95, 0xd8
-#define FN_COLOR 0xff, 0xb5, 0x11
-
-void matrix_init_user(void) {
-    rgb_matrix_disable();
-}
+#define HFN_COLOR 0x30, 0x99, 0x75
+#define TFN_COLOR 0x58, 0xb3, 0x68
+#define SFN_COLOR 0xda, 0xd8, 0x73
 
 enum custom_keycodes {
-    SW_WIN = SAFE_RANGE,
-    SW_DSC,
+    LT_FN = SAFE_RANGE,
+    MF_SFN,
+    SW_WIN,
+    SW_DSC
 };
 
+/* Timers for tap/hold fn layer changes */
+static uint16_t fn_layer_tap_timer;
+
+/* Disable lights after 10 minutes. */
+#define RGB_TIMEOUT 10 * 10 * 1000
+static uint16_t idle_timer = 0;
+static bool rgb_matrix_enabled = true;
+
+void matrix_scan_user(void) {
+    if (idle_timer == 0) idle_timer = timer_read();
+    if (timer_elapsed(idle_timer) > RGB_TIMEOUT && rgb_matrix_enabled) {
+        rgb_matrix_enabled = false;
+        rgb_matrix_disable_noeeprom();
+    }
+}
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (!rgb_matrix_enabled) rgb_matrix_enable_noeeprom();
+        idle_timer = timer_read();
+    } 
     switch (keycode) {
-        case SW_WIN:
+        case LT_FN:
             if (record->event.pressed) {
-                // pressed
+                fn_layer_tap_timer = timer_read();
+                layer_on(HFN);
             } else {
-                // released
+                layer_off(HFN);
+                if (timer_elapsed(fn_layer_tap_timer) < TAPPING_TERM) {
+                    layer_invert(TFN);
+                }
+            }
+            break;
+        case MF_SFN:
+            if (record->event.pressed) {
+                layer_on(SFN);
+            } else {
+                layer_off(SFN);
+                layer_off(TFN);
+            }
+            break;
+        case SW_WIN:
+            if (!record->event.pressed) {
                 layer_invert(WIN);
-                layer_off(FN);
+                layer_off(SFN);
             }
             break;
         case SW_DSC:
-            if (record->event.pressed) {
-                // pressed
-            } else {
-                // released
+            if (!record->event.pressed) {
                 layer_invert(DSC);
-                layer_off(FN);
+                layer_off(SFN);
             }
             break;
     }
     return true;
 }
 
-bool caps_word = false;
+static bool caps_word = false;
 
 void caps_word_set_user(bool active) {
     caps_word = active;
 }
 
 void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t layer = get_highest_layer(layer_state | default_layer_state);
+    if (layer > DSC) {
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+            for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+                if (index >= 0 && index <= 88 && index != NO_LED &&
+                    keymap_key_to_keycode(layer, (keypos_t){col, row}) > KC_TRNS) {
+                    switch (layer) {
+                        case TFN:
+                            rgb_matrix_set_color(index, TFN_COLOR);
+                            break;
+                        case HFN:
+                            rgb_matrix_set_color(index, HFN_COLOR);
+                            break;
+                        case SFN:
+                            rgb_matrix_set_color(index, SFN_COLOR);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
     if (host_keyboard_led_state().caps_lock || caps_word) {
         rgb_matrix_set_color(30, RGB_WHITE);
     }
 
-    if (IS_LAYER_ON(TFN)) {
+    if (IS_LAYER_ON(HFN)) {
+        rgb_matrix_set_color(59, HFN_COLOR);
+    } else if (IS_LAYER_ON(TFN)) {
         rgb_matrix_set_color(59, TFN_COLOR);
-    } else if (IS_LAYER_ON(FN)) {
-        rgb_matrix_set_color(59, FN_COLOR);
+    } else if (IS_LAYER_ON(SFN)) {
+        rgb_matrix_set_color(59, SFN_COLOR);
     }
 
     if (IS_LAYER_ON(WIN)) {
@@ -111,15 +170,22 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
 }
 
-
-bool led_update_user(led_t led_state) {
-    if (led_state.caps_lock) {
-        rgb_matrix_set_color(4, 0xff, 0xff, 0x00);
-    }
-    return true;
+void suspend_power_down_kb(void) {
+    rgb_matrix_set_suspend_state(true);
 }
 
-LEADER_EXTERNS();
+void suspend_wakeup_init_kb(void) {
+    rgb_matrix_set_suspend_state(false);
+}
+
+/* bool led_update_user(led_t led_state) { */
+/*     if (led_state.caps_lock) { */
+/*         rgb_matrix_set_color(4, 0xff, 0xff, 0x00); */
+/*     } */
+/*     return true; */
+/* } */
+
+// LEADER_EXTERNS();
 
 /* void matrix_scan_user(void) { */
 /*     LEADER_DICTIONARY() { */
@@ -157,35 +223,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,
   KC_LCTL, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_PGUP,
   KC_LSFT, KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_PGDN,
-  KC_LALT, MO(TFN),  KC_LGUI,                                KC_SPC,                                 KC_RGUI,  KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+  KC_LALT, LT_FN,    KC_LGUI,                                KC_SPC,                                 KC_RGUI,  KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN] = LAYOUT(
   KC_GRV,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_ESC,
   KC_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,
   KC_LCTL, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_PGUP,
   KC_LSFT, KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_PGDN,
-  KC_LGUI, MO(TFN),  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+  KC_LGUI, LT_FN,    KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [DSC] = LAYOUT(
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  PB_1,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            PB_2,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  PB_3,
-  PB_4,     _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_F20,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_F21,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  KC_F22,
+  KC_F23,   _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
+
+[HFN] = LAYOUT(
+  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______, _______,
+  C(KC_TAB),_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, KC_INS,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           KC_HOME,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______, KC_END,
+  _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______, _______),
 
 [TFN] = LAYOUT(
-  TG(FN),   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     _______,    _______,
-  C(KC_TAB),_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    KC_INS,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                KC_HOME,
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              C(KC_UP),   KC_END,
-  _______,  _______,  _______,                                _______,                                _______,  _______,  C(KC_LEFT), C(KC_DOWN), C(KC_RIGHT)),
-
-[FN] = LAYOUT(
-  _______,  SW_WIN,   SW_DSC,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,  _______,  _______,  _______,  _______,
+  _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  TG(TFN),
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,
   KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BRID,  KC_BRIU,  _______,            _______,
-  _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,  _______,  _______,  _______,  KC_MPRV,  KC_MNXT,  KC_MPLY,  _______,            _______,  _______,
-  _______,  TG(FN),   _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
+  _______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  _______,  _______,  KC_MPRV,  KC_MNXT,  KC_MPLY,  _______,            _______,  _______,
+  _______,  MF_SFN,   _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
+
+[SFN] = LAYOUT(
+  QK_BOOT,  KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,   KC_F23,   _______,  _______,
+  _______,  _______,  SW_WIN,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  _______,  _______,  SW_DSC,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+  _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______),
 
 [31] = LAYOUT(
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,
