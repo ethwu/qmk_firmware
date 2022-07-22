ff97b5e12c86fd94be435df93952bfc6fb5cfb21
Ethan Wu <ethanwu@cs.ucsb.edu>
2022-07-21 18:52:41 -0700

    Stack rgb fn indicators

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/ethwu/keymap.h
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/ethwu/keymap.h
@@ -14,6 +14,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include QMK_KEYBOARD_H
+
 enum Layers {
 	// Base layer for macOS.
 	MAC,
